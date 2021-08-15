33dab73a6f8e3b787c7b8a4b4385f5735459b4f6
Hoang Anh Nguyen <hoanganh.nguyen@the-hash.com>
2021-08-15 17:44:14 +0700

    update code

--- script/main.js
+++ script/main.js
@@ -295,6 +295,8 @@ const animationTimeline = () => {
   // tl.seek("currentStep");
   // tl.timeScale(10);
 
+
+
   // Restart Animation on click
   const replyBtn = document.getElementById("replay");
   replyBtn.addEventListener("click", () => {
